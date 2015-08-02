c17f82d4cb1bd70ed5bc7e592394135fa24eb86a
Lars Moelleken <lmoelleken@menadwork.com>
2015-08-02 19:43:59 +0200

    [*]: doc / phpdoc fixes | by https://github.com/stellis/paris/commit/c9f00c4573a3f8e3b8f9b754b790007ef98d9e68

--- paris.php
+++ paris.php
@@ -48,6 +48,11 @@
      * You shouldn't need to interact with this class
      * directly. It is used internally by the Model base
      * class.
+     *
+     * @method void setClassName($class_name)
+     * @method static \ORMWrapper forTable($table_name, $connection_name = parent::DEFAULT_CONNECTION)
+     * @method \Model findOne($id=null)
+     * @method Array findMany()
      */
     class ORMWrapper extends ORM {
 
@@ -168,9 +173,13 @@
      * Model base class. Your model objects should extend
      * this class. A minimal subclass would look like:
      *
-     * class Widget extends Model {
-     * }
+     * class Widget extends Model { }
      *
+     * @method void setOrm($orm)
+     * @method $this setExpr($property, $value = null)
+     * @method bool isDirty($property)
+     * @method bool isNew()
+     * @method Array asArray()
      */
     class Model {
 
